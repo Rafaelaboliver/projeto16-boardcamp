779b0882768c9a364f64740489d5a484cb50eb30
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-02-10 20:34:18 -0600

    fix: POST/customers cpf pattern

--- src/schemas/customerSchemas.js
+++ src/schemas/customerSchemas.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const customerSchema = joi.object({
     name: joi.string().empty().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(11).required(),
+    cpf: joi.string().pattern(/^[0-9]+$/).length(11).required(),
     birthday: joi.date().max('now').required()
     });
     
